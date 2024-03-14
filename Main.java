d619611c71dfe04ac96a78af83abe7e16f8189ae
Prince <prince02@gmail.com>
2024-03-14 11:52:37 +0530

    Edited by prince

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ public class Main {
   static void myMethod() {
     System.out.println("I just got executed!");
   }
+  static void prince(){
+	  System.out,println("Prince");
+  }
+  static void sam(){
+	  System.out.println("Sam");
+  }
 
   public static void main(String[] args) {
     myMethod();

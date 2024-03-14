02ecf4b7f58eec7fc4fd05ebbde75066d29d87d1
Ponraj Marikannan <ponraj.marikannan@itp.objectfrontier.com>
2024-03-14 11:49:54 +0530

    Edited By Raj!

--- Main.java
+++ Main.java
@@ -2,8 +2,16 @@ public class Main {
   static void myMethod() {
     System.out.println("I just got executed!");
   }
+  static void Raj(){
+	  s.o.p("Raj!");
+  }
+  static void Kutty(){
+	  s.o.p("Kutty!");
+  }
 
   public static void main(String[] args) {
     myMethod();
+    Raj();
+    Kutty();
   }
 }

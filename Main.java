456545c606aec143fe80ccefda1cece103061cdb
Nandha <nandhakumaran6403@gmail.com>
2024-03-14 12:04:00 +0530

    Updated By Nandha

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 public class Main {
   static void myMethod() {
-    System.out.println("I just got executed!");
+    System.out.println("Updated By Nandhakumaran");
   }
+
+  static void Method(){
+          S.o.p("My Name is Nandhakumaran");
+  }
+
+
+  static voidmyMethod1(){
+          S.o.p("We are Team4");
+  }
+
+
 
   public static void main(String[] args) {
     myMethod();
+    myMethod1();
+
   }
 }
+
